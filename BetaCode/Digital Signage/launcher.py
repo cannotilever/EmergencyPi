3b51189a670af75e0c4433ce14ab19812b9a23e6
cannotilever <51347140+cannotilever@users.noreply.github.com>
2019-07-04 15:48:35 -0400

    Reorganization and 1st backup of Digital Signage

---
+++ BetaCode/Digital Signage/launcher.py
@@ -0,0 +1,9 @@
+#Launcher Script for EmergencyPi system
+#No current tasks to launch
+import os
+import time
+time.sleep(10)
+#os.system("chromium-browser --kiosk https://docs.google.com/presentation/d/e/2PACX-1vQj_fprOF5JZJypSNmieEJkrMtfFJBUxENsXTUdyCddgfc3lHV3OUyy6bwp3j4HAL_pHV7klZSKzbA-/pub?start=true&loop=true&delayms=3000 &")
+os.system("touch bruh.moment")
+print ("bruh")
+exit()
