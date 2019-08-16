a313f6112f5a5bd97ca6eaaad46301e723b7a66f
cannotilever <51347140+cannotilever@users.noreply.github.com>
2019-08-16 01:04:49 -0400

    Added prompt to run windows endpoint

--- Production/Windows/MessageSpawn.py
+++
@@ -1,2 +0,0 @@
-from tkinter import messagebox
-messagebox.showerror("Emergency Alert Message", "The local listener has crashed 3 times. Rebooting is recommended. If this problem persists, contact IT.")
