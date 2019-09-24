9d43b7c07e78313002ef5a825891a7f00905be2c
cannotilever <51347140+cannotilever@users.noreply.github.com>
2019-09-24 11:02:37 -0400

    Removed Windows activaiton prompt

--- Block 1/Production/Windows/EmergencyAlertEndpoint/child.py
+++ Block 1/Production/Windows/EmergencyAlertEndpoint/child.py
@@ -7,32 +7,15 @@ import win32con
 import math
 import subprocess
 parentCrashCounter = 0
-def OnStartup():
-    print("  ---------------------------------------------")
-    print("| Welcome to the HVRHS Emergency Alert System. |")
-    print("  ---------------------------------------------")
-    print("")
-    userInput = input("Would you like to enable the alert service on your computer for this session? (Y/N): ")
-    if (userInput == "Y") or (userInput == "y"):
-        print("")
-        print("The Emergency Alert System Endpoint is now active!")
-        print("This window will hide itself automatically in 5 seconds...")
-        Minimize = win32gui.GetForegroundWindow()
-        time.sleep(5)
-        win32gui.ShowWindow(Minimize, win32con.SW_MINIMIZE)
-    else:
-        if (userInput == "N") or (userInput == "n"):
-            print("")
-            print("The Emergency Alert System Endpoint is now deactivated for the duration of this session. "
-                  "Terminating in 5 seconds...")
-            time.sleep(5)
-            exit()
-        print("")
-        print("INVALID INPUT DETECTED. Please type Y or N!")
-        print("")
-        OnStartup()
-
-OnStartup()
+print("  ---------------------------------------------")
+print("| Welcome to the HVRHS Emergency Alert System. |")
+print("  ---------------------------------------------")
+print("")
+print("The Emergency Alert System Endpoint is now active!")
+print("This window will hide itself automatically in 2 seconds...")
+Minimize = win32gui.GetForegroundWindow()
+time.sleep(2)
+win32gui.ShowWindow(Minimize, win32con.SW_MINIMIZE)
 while True:
     time.sleep(3)
     if ("py.exe" in (p.name() for p in psutil.process_iter())):
